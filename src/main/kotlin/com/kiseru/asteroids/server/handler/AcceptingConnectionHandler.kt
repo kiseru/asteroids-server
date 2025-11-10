1a8ced498c83652414592c25cd71daa1944a7095
Alexandr Kiselev <kis.alex97@gmail.com>
2025-11-10 16:29:07 +0300

    refactored AcceptingConnectionHandler.kt

--- src/main/kotlin/com/kiseru/asteroids/server/handler/AcceptingConnectionHandler.kt
+++ src/main/kotlin/com/kiseru/asteroids/server/handler/AcceptingConnectionHandler.kt
@@ -7,8 +7,18 @@ import java.util.concurrent.BlockingQueue
 class AcceptingConnectionHandler(private val connectionQueue: BlockingQueue<Socket>) {
 
     fun handle(port: Int) {
+        require(port in 1..65535) { "Port must be between 1 and 65535, but was $port" }
+
         val serverSocket = ServerSocket(port)
-        generateSequence { serverSocket.accept() }
-            .forEach { connectionQueue.put(it) }
+
+        try {
+            generateSequence { serverSocket.accept() }
+                .forEach { connectionQueue.put(it) }
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw e
+        } finally {
+            serverSocket.close()
+        }
     }
 }
